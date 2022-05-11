30f0e99ec9de7a41256af74eda117c77afb154e2
Phil Broadley <philbro1992@hotmail.co.uk>
2022-05-11 21:26:25 +0100

    change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd}) => {
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onClick} />
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   )
 }
